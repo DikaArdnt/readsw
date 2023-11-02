e017dc4c7a9e56e27a58db660b2fc5ac62e930fd
DikaArdnt <=>
2023-11-03 00:05:29 +0700

    buat diri sendiri

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 import "dotenv/config"
+import Color from "./lib/color.js"
+import serialize from "./lib/serialize.js"
 
 import makeWASocket, { delay, useMultiFileAuthState, fetchLatestWaWebVersion, makeInMemoryStore, jidNormalizedUser, PHONENUMBER_MCC, DisconnectReason } from "@whiskeysockets/baileys"
 import pino from "pino"
 import { Boom } from "@hapi/boom"
 import fs from "fs"
+import util from "util"
+import { exec } from "child_process"
 
 const logger = pino({ timestamp: () => `,"time":"${new Date().toJSON()}"` }).child({ class: "hisoka" })
 logger.level = "fatal"
 
-const useStore = process.argv.includes('--store')
 const usePairingCode = process.env.PAIRING_NUMBER
 
-const store = useStore ? makeInMemoryStore({ logger }) : undefined
+const store = makeInMemoryStore({ logger })
+store.readFromFile("./session/store.json")
 
 const startSock = async () => {
    const { state, saveCreds } = await useMultiFileAuthState("./session")
@@ -29,7 +33,7 @@ const startSock = async () => {
       getMessage
    })
 
-   if (useStore) store.bind(hisoka.ev)
+   store.bind(hisoka.ev)
 
    // login dengan pairing
    if (usePairingCode && !hisoka.authState.creds.registered) {
@@ -81,7 +85,7 @@ const startSock = async () => {
                console.error("Nedd Multi Device Version, please update and rescan again...")
                fs.rmdirSync("./session")
                break
-            default: 
+            default:
                console.log("Aku ra ngerti masalah opo iki")
                process.exit(1)
          }
@@ -95,15 +99,102 @@ const startSock = async () => {
    // write session kang
    hisoka.ev.on("creds.update", saveCreds)
 
-   // bagian pepmbaca status ono ng kene
-   hisoka.ev.on("messages.upsert", async ({ messages }) => {
-      let message = messages[0]
-
-      if (message.key && !message.key.fromMe && message.key.remoteJid === "status@broadcast") {
-         await hisoka.readMessages([message.key])
-         await hisoka.sendMessage(jidNormalizedUser(hisoka.user.id), { text: `Read Story @${message.key.participant.split("@")[0]}`, mentions: [message.key.participant] }, { quoted: message })
+   // contacts from store
+   hisoka.ev.on("contacts.update", (update) => {
+      for (let contact of update) {
+         let id = jidNormalizedUser(contact.id)
+         if (store && store.contacts) store.contacts[id] = { id, name: contact?.notify, verifiedName: contact?.verifiedName }
       }
    })
+
+   hisoka.ev.on("contacts.upsert", (update) => {
+      for (let contact of update) {
+         if (store && store.contacts) store.contacts[contact.id] = { id: contact.id, name: contact?.name, verifiedName: contact?.verifiedName }
+      }
+   })
+
+   // bagian pepmbaca status ono ng kene
+   hisoka.ev.on("messages.upsert", async ({ messages }) => {
+      let m = await serialize(hisoka, messages[0])
+      try {
+         let quoted = m.isQuoted ? m.quoted : m
+
+         // status self apa publik
+         if (!process.env.PUBLIC && !m.isOwner) return
+
+         // mengabaikan pesan dari bot
+         if (m.isBot) return
+
+         // memunculkan ke log
+         if (m.message && !m.isBot) {
+            console.log(Color.black(Color.bgWhite("FROM")), Color.black(Color.bgGreen(m.pushName)), Color.black(Color.yellow(m.sender)) + "\n" + Color.black(Color.bgWhite("IN")), Color.black(Color.bgGreen(m.isGroup ? "Group" : "Private")) + "\n" + Color.black(Color.bgWhite("MESSAGE")), Color.black(Color.bgGreen(m.body || m.type)))
+         }
+
+         // untuk membaca pesan status
+         if (m.key && !m.key.fromMe && m.key.remoteJid === "status@broadcast") {
+            await hisoka.readMessages([m.key])
+            await hisoka.sendMessage(jidNormalizedUser(hisoka.user.id), { text: `Read Story @${sender.participant.split("@")[0]}`, mentions: [sender] }, { quoted: m, ephemeralExpiration: m.expiration })
+         }
+
+         // command
+         switch (m.command) {
+            case "quoted": case "q":
+               if (!m.isQuoted) throw "Reply Pesan"
+               try {
+                  var message = await serialize(hisoka, (await store.loadMessage(m.from, m.quoted.id)))
+                  if (!message.isQuoted) throw "Pesan quoted gaada"
+                  await m.reply({ forward: message.quoted })
+               } catch (e) {
+                  throw "Pesan gaada"
+               }
+               break
+
+            case "rvo":
+               if (!quoted.msg.viewOnce) throw "Reply Pesan Sekali Lihat"
+               quoted.msg.viewOnce = false
+               await m.reply({ forward: quoted })
+               break
+
+            default:
+               // eval
+               if ([">", "eval", "=>"].some(a => m.body?.toLowerCase()?.startsWith(a)) && m.isOwner) {
+                  let evalCmd = ""
+                  try {
+                     evalCmd = /await/i.test(m.text) ? eval("(async() => { " + m.text + " })()") : eval(m.text)
+                  } catch (e) {
+                     evalCmd = e
+                  }
+                  new Promise(async (resolve, reject) => {
+                     try {
+                        resolve(evalCmd);
+                     } catch (err) {
+                        reject(err)
+                     }
+                  })
+                     ?.then((res) => m.reply(util.format(res)))
+                     ?.catch((err) => m.reply(util.format(err)))
+               }
+
+               // exec
+               if (["$", "exec"].some(a => m.body?.toLowerCase()?.startsWith(a)) && m.isOwner) {
+                  try {
+                     exec(m.text, async (err, stdout) => {
+                        if (err) return m.reply(util.format(err))
+                        if (stdout) return m.reply(util.format(stdout))
+                     })
+                  } catch (e) {
+                     await m.reply(util.format(e))
+                  }
+               }
+         }
+      } catch (err) {
+         await m.reply(util.format(err))
+      }
+   })
+
+   setInterval(() => {
+      store.writeToFile("./session/store.json")
+   }, 10 * 1000) // tiap 10 detik
 
    process.on("uncaughtException", console.error)
    process.on("unhandledRejection", console.error)
