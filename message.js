1ddfd6ebf6e923ee1d686cf0b795bf6e75e9d2a2
DikaArdnt <=>
2023-12-07 03:00:55 +0700

    wajs

--- message.js
+++ message.js
@@ -3,7 +3,7 @@ import * as func from './lib/function.js'
 import Color from './lib/color.js'
 
 import util from 'util'
-import * as wajs from 'whatsapp-web.js'
+import wajs from 'whatsapp-web.js'
 import { exec } from 'child_process'
 import { performance } from 'perf_hooks'
 
